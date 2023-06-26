e1c4d2c48207f1cff94feffe900ef7fcf9911a47
Shivani Shrivastava <100347899+shivani25shri@users.noreply.github.com>
2023-06-26 12:17:38 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,61 @@
+import speech_recognition as april
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = april.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with april.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'april' in command:
+                command = command.replace('april', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_april():
+    command = take_command()
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing')
+        print('playing')
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk('Current time is ' + time)
+    elif 'wikipedia' in command:
+        person = command.replace('who the heck is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('Please say the command again.')
+
+
+while True:
+    run_april()
